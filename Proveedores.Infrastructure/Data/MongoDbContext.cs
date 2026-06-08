5543aa32b00c26f261d1f230d6825b17f221cbc6
Viselito07 <egonate@unicesar.edu.co>
2026-06-08 14:15:52 -0500

    fix: TLS config for Atlas, update password, Docker port mapping

--- Proveedores.Infrastructure/Data/MongoDbContext.cs
+++ Proveedores.Infrastructure/Data/MongoDbContext.cs
@@ -1,4 +1,6 @@
-﻿using MongoDB.Driver;
+﻿using System.Net.Security;
+using System.Security.Authentication;
+using MongoDB.Driver;
 using Proveedores.Domain.Entities;
 
 namespace Proveedores.Infrastructure.Data;
@@ -9,7 +11,14 @@ public class MongoDbContext
 
     public MongoDbContext(string connectionString, string databaseName)
     {
-        var client = new MongoClient(connectionString);
+        var settings = MongoClientSettings.FromConnectionString(connectionString);
+        settings.SslSettings = new SslSettings
+        {
+            EnabledSslProtocols = SslProtocols.Tls12 | SslProtocols.Tls13,
+            CheckCertificateRevocation = false,
+            ServerCertificateValidationCallback = (sender, certificate, chain, errors) => true
+        };
+        var client = new MongoClient(settings);
         _database = client.GetDatabase(databaseName);
     }
 
